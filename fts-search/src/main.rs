873f2b4bd9fa406f9d074c31b72e67bcc5c76c47
Dai <ddao@ualberta.ca>
2021-05-13 10:04:21 -0400

    created indexer

--- fts-search/src/main.rs
+++ fts-search/src/main.rs
@@ -4,6 +4,11 @@ use std::io::{self, BufReader};
 use std::fs::File;
 use std::time::{Duration, Instant};
 use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+
+extern crate rust_stemmers;
+use rust_stemmers::{Algorithm, Stemmer};
 
 
 #[derive(Debug, Deserialize, PartialEq)]
@@ -12,6 +17,7 @@ struct Document {
     url: String,
     #[serde(rename = "abstract", default)]
     text: String,
+    id: Option<usize>
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
@@ -21,7 +27,10 @@ struct Feed {
 }
 
 fn get_feed(bufread: BufReader<File>) -> Result<Feed, DeError> {
-    let feed: Feed = from_reader(bufread)?;
+    let mut feed: Feed = from_reader(bufread)?;
+    for (id, doc) in feed.documents.iter_mut().enumerate() {
+        doc.id = Some(id);
+    }
     Ok(feed)
 }
 
@@ -69,26 +78,86 @@ fn tokenize(text: String) -> Vec<String> {
     let raw_tokens : Vec<&str> = text.split(" ").collect();
     let mut tokens = Vec::new();
     for rt in raw_tokens {
-        if is_word(rt.to_string()) && !is_numeric(rt.to_string()) {
-            tokens.push(rt.to_lowercase().to_string());
+        if is_word(rt.to_string()) && !rt.is_empty() {
+            tokens.push(rt.to_string());
         }
     }
     return tokens
 }
 
 
+fn lowercase_filter(tokens: Vec<String>) -> Vec<String> {
+    let mut out = Vec::new();
+    for rt in tokens {
+        out.push(rt.to_lowercase());
+    }
+    return out
+}
+
+
+fn stopword_filter(tokens: Vec<String>) -> Vec<String> {
+    let stop_words =  vec!["a", "and", "be", "have", "i", "in", "of", "that", "the", "to"];
+    let mut out = Vec::new();
+    for rt in tokens {
+        if !stop_words.contains(&&*rt) {
+            out.push(rt);
+        } 
+    }
+    return out
+}
+
+
+fn stemmer_filter(tokens: Vec<String>) -> Vec<String> {
+    let en_stemmer = Stemmer::create(Algorithm::English);
+    let mut out = Vec::new();
+    for rt in tokens {
+        out.push(en_stemmer.stem(&&*rt).to_string());
+    }
+    return out
+}
+
+
+fn analyze(text: String) -> Vec<String> {
+    let mut tokens = tokenize(text);
+    tokens = lowercase_filter(tokens);
+    tokens = stopword_filter(tokens);
+    tokens = stemmer_filter(tokens);
+    return tokens
+}
+
+
+fn make_index(docs : Vec<Document>) -> HashMap<String, HashSet<usize>> {
+    let mut index : HashMap<String, HashSet<usize>> = HashMap::new();
+    for doc in docs {
+        let tokens = analyze(doc.text);
+        for token in tokens {
+            // cloning to own the value
+            index.entry(token.clone()).or_insert(HashSet::new());
+            index.get_mut(&token).unwrap().insert(doc.id.unwrap());
+        }
+    }
+    return index
+}
+
+
 fn main() -> io::Result<()> {
     // let f = File::open("enwiki-latest-abstract1.xml")?;
-    // let reader = BufReader::new(f);
-    // let feed = get_feed(reader).unwrap();
+    let f = File::open("small.xml")?;
+    let reader = BufReader::new(f);
+    let feed = get_feed(reader).unwrap();
 
     // let start = Instant::now();
     // let docs = search_regex(feed, "cat".to_string());
     // println!("Results {:?}", docs);
     // let duration = start.elapsed();
     // println!("Time elapsed in search: {:?}", duration);
-    let tokens = tokenize("I love my baby !! 123 31Q !".to_string());
-    println!("Tokens are {:?}", tokens);
+    
+    
+    // let tokens = analyze("I Loves my baby !! 123 31Q !".to_string());
+    // println!("Tokens are {:?}", tokens);
+
+    let index = make_index(feed.documents);
+    println!("Index {:?}", index);
 
     Ok(())
 }
